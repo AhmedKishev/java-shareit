fe7f6ab01ca0c7a310f133cf33732870ffc6d435
AhmedKishev <kishevahmed0@yandex.ru>
2025-07-02 03:21:35 +0300

    add request, tests and multiserver architecture

--- server/src/main/java/ru/practicum/shareit/booking/dto/BookingDtoShort.java
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingDtoShort.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.booking.dto;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import ru.practicum.shareit.status.Status;
 
